47d8596864104bdc0efcd07366cbea5b9963efef
Michelle <michelle@Michelles-Air.attlocal.net>
2021-03-24 20:06:32 -0400

    initial commit testing project folder and repository connection

---
+++ atm.js
@@ -0,0 +1,9 @@
+//account imports into here
+//this imports all functions into index
+//contain the following functions
+    //getBalance
+    //withdraw
+    //deposit
+    //validatePin
+//must be able to interact until exit is selected
+//account for any mistakes when information is entered
---
+++ index.js
@@ -0,0 +1,2 @@
+//final imported into file
+//this will contain the user menu that interacts with the atm
