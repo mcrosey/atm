f1cb84151b2ef729b1be58a75255c240c2c42351
Michelle Chute <michellechute@icloud.com>
2021-03-24 21:32:08 -0400

    lines 14-17 of atm js file  returned balance after successful prompt and input entry. successful export and import from account js file

--- atm.js
+++ atm.js
@@ -7,3 +7,31 @@
     //validatePin
 //must be able to interact until exit is selected
 //account for any mistakes when information is entered
+
+
+//initial prompt should list all the options including exti option
+// let displayBalance = module.exports.balance;
+const prompt = require('prompt-sync')();
+console.log ('check balance')
+const {balance} = require('./account');
+console.log(balance);
+
+
+
+
+// function getBalance (input){
+     
+//     console.log(balance);
+// }
+
+// function withdraw (input){
+
+// }
+
+// function deposit (input){
+
+// }
+
+// function validatePin (input){
+
+// }
