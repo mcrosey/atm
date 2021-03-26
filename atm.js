08269cd447492497be1d154ce4e9fb8c96f671de
Michelle Chute <michellechute@icloud.com>
2021-03-25 22:38:56 -0400

    added switch case lines 23-40 to pass user input to select different functions. not fully functioning

--- atm.js
+++ atm.js
@@ -15,16 +15,21 @@ function withdraw(withdrawAmount){
 
 function deposit(depositAmount){
     account.balance = account.deposit + depositAmount;
+    console.log(account.balance);
 }
 
 //console.log(account.balance);
 
 function validatePin (userInput){
 let pinAttempt = userInput;
-if (pinAttempt == account.pin){
-    return true;
+if(pinAttempt != account.pin){
+    return "please re-enter pin";
 }else{
-    return false;
+    return "thank you";
 }
+console.log(validatePin);
 }
 module.exports.validate = validatePin;
+module.exports.getBalance = getBalance;
+module.exports.withdraw = withdraw;
+module.exports.deposit = deposit;
--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-//final imported into file
-//this will contain the user menu that interacts with the atm
-
 const { pin, balance } = require("./account")
 const prompt = require('prompt-sync')();
 
 // menu
-// enterpin
-// validatepin
-// or reenter pin
+// enterpin~
+// validatepin~
+// or reenter pin ... prompt works does not request reentery
 // list of choice
 // view balance
 // make a withdrawl
@@ -17,6 +14,27 @@ const prompt = require('prompt-sync')();
 console.log("welcome");
 console.log("please enter pin");
 const enterPin = prompt();
-const {validate} = require('./atm');
+const {validate, getBalance, deposit, withdraw} = require('./atm');
 let result = validate(enterPin);
 console.log(result);
+console.log('choose menu item');
+console.log('1 check balance \n2 make a deposit \n3 make a withdrawl');
+const makeChoice = prompt();
+switch(makeChoice) {
+case "1":
+    getBalance;
+    console.log(balance);
+    break;
+case "2":
+    let depositAmount = prompt("How much would you like to deposit?")
+    parseInt(depositAmount);
+    deposit;
+    console.log(balance);
+    break;
+case "3":
+    withdraw;
+    console.log(balance);
+    break;
+default:
+    break;
+}
